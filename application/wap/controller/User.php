6abcfbb90fd9f63dc77795986ea6ca16ae901a2d
css <2523101037@qq.com>
2017-01-06 13:49:24 +0800

    SJAGDFUSYJHGDUYSJDG

--- application/wap/controller/User.php
+++ application/wap/controller/User.php
@@ -204,15 +204,17 @@ class User extends WeixinBase
             }
         }
 		$res =  Users::myrebateLst($uid,$where);
-		$commission =  Users::myInfo($uid);
 		$row =  UsersRebate::countRebate($uid);
         if ($res['error_code'] == 0){
             $data = $res['data'];
+			foreach($data as $v){
+				$commission = $v['commission'];
+			}
         }else{
             $data = '';
         }
         $this->assign('data',$data);
-        $this->assign('commission',$commission['data']);
+        $this->assign('commission',$commission);
         $this->assign('row',$row);
 		$this->assign('type',$type);
         return $this->fetch();
